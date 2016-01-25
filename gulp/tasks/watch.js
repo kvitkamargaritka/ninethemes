3c7172cfbdba85fa0c843978b522302f6fc01ee3
kvitka <kvitka@kvitkas-MacBook-Pro.local>
2016-01-25 23:41:38 +0200

    add user-menu

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -8,7 +8,8 @@ gulp.task('watch', [
     'copy:watch',
     'html:watch',
     'font:watch',
-    'js:watch'
+    'js:watch',
+    'jade:watch'
 ]);
 
 
@@ -16,4 +17,4 @@ gulp.task('delete', function (cb) {
     rimraf('./'+config.dest.root, cb);
 });
 gulp.task('default', ['server', 'watch'], function() {});
-gulp.task('build', ['html','font','sprite','copy','js','sass'], function() {});
+gulp.task('build', ['html','font','sprite','copy','js','sass','jade'], function() {});
