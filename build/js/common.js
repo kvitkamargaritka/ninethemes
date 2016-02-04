227f4189f1118c8da8d48d96394d3a3c8ff09cd2
kvitka <kvitka@kvitkas-MacBook-Pro.local>
2016-02-05 00:54:55 +0200

    owl-carousel done

--- build/js/common.js
+++ build/js/common.js
@@ -1,5 +1,34 @@
 $(document).ready(function() {
 
-	// go js
+    //owl-footer
+    $('.owl-footer').owlCarousel({
+        items : 7,
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        pagination : false,
+        navigation : true,
+        navigationText: [
+            "&larr;",
+            "&rarr;"
+        ]
+    });
+
+    //owl-review
+    $('.owl-review').owlCarousel({
+        singleItem:true,
+        navigation: true,
+        navigationText : ["",""]
+    });
+
+    //roundabout
+    $('.choose-bike__list').roundabout ({
+        autoplay: true,
+        autoplayDuration: 5000,
+        minOpacity: 1,
+        minScale: 0.55,
+        btnNext: ".next",
+        btnPrev: ".prev",
+        autoplayPauseOnHover: true
+    });
 
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,7 @@
 //=include lib/jquery.js
 //=include lib/slick.min.js
+//=include lib/owl.carousel.min.js
+//=include lib/jquery.roundabout.min.js
 //=include common.js
+
+
